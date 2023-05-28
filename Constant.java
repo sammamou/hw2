8da1acddcf9c2f07d7885a0cabf3dad63de63dd6
sammamou <131761895+sammamou@users.noreply.github.com>
2023-05-28 19:01:00 +0300

    Add files via upload

--- Constant.java
+++ Constant.java
@@ -1,16 +1,31 @@
 public class Constant extends Function{
-    private int x;
+    private double x;
 
-    Constant(int x){
+    Constant(double x){
         this.x= x;
     }
 
-    public int getX() {
+    public double getX() {
         return x;
     }
 
     @Override
     public double valueAt(double x) {
         return this.getX();
+    }
+
+    @Override
+    public String toString() {
+        return "(" +getX()+ ")";
+    }
+
+    @Override
+    public String derivative() {
+        return "0";
+    }
+
+    @Override
+    public String taylorPolynomial(int n) {
+        return "(" +getX()+ ")";
     }
 }
