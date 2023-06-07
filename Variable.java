428b7e359a50e78ca2effe7cd2cef70c8a48c345
sammamou <131761895+sammamou@users.noreply.github.com>
2023-06-07 17:27:25 +0300

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,22 +1,6 @@
-public class Variable extends Function {
-    private String symbol;
-
-    public Variable(String symbol) {
-        this.symbol = symbol;
-    }
-
-    @Override
-    public double valueAt(double x) {
-        return x;
-    }
-
-    @Override
-    public String toString() {
-        return symbol;
-    }
-
-    @Override
-    public Function derivative() {
-        return new Constant(1);
-    }
-}
+df_with_dangerous_plane = df[df["type"].isin(most_dangerous_plane["type"])]
+dangerous_operator_liste = df_with_dangerous_plane["operator"].unique()
+df_with_dangerous_operator = df[df["operator"].isin(dangerous_operator_liste)].sort_values("operator").reset_index().drop("index", axis =1)
+most_dangerous_operator = df_with_dangerous_operator.groupby("operator").agg(number_accident = ("cat", "count")).reset_index().sort_values("number_accident", ascending = False).head(3)
+dangerous_operator_graph = sns.catplot(x="operator", y="number_accident", kind="bar", data=most_dangerous_operator);
+dangerous_operator_graph.fig.suptitle('3 most dangerous operators')
